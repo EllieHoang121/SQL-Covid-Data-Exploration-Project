9526e9cf335d133a011da83f154330c69f9d3bfa
IamEllie <144719446+EllieHoang121@users.noreply.github.com>
2023-10-14 17:29:25 +0300

    Add files via upload

--- Covid_profolio_project.sql
+++ Covid_profolio_project.sql
@@ -11,7 +11,8 @@ ORDER BY location, date
 
 -- Calculating the likelihood of dying in the United States
 
-SELECT location, date, total_cases, total_deaths, CONCAT((ROUND((total_deaths/total_cases)*100, 2)), '%') AS death_percentage
+SELECT location, date, total_cases, total_deaths, 
+CONCAT((ROUND(((CAST(total_deaths AS bigint)/CAST(total_cases AS bigint))*100), 2)), '%') AS death_percentage
 FROM covid_death
 WHERE location LIKE '%States%'
 AND continent IS NOT NULL
@@ -19,7 +20,8 @@ ORDER BY location, date
 
 -- Countries with highest infected percentages compare to population
 
-SELECT location, population, MAX(total_cases) AS maximum_infected_cases, CONCAT(MAX(ROUND((total_cases/population)*100, 5)), '%') AS highest_infected_percentage
+SELECT location, population, MAX(total_cases) AS maximum_infected_cases, 
+CONCAT(MAX(ROUND((total_cases/population)*100, 5)), '%') AS highest_infected_percentage
 FROM covid_death
 WHERE continent IS NOT NULL
 GROUP BY location, population
@@ -27,7 +29,8 @@ ORDER BY highest_infected_percentage desc
 
 -- Countries with total deaths and its highest death rate
 
-SELECT location, SUM(CAST(total_deaths AS int)) AS total_deaths, CONCAT(MAX(ROUND((total_deaths/total_cases)*100, 2)), '%') AS highest_death_rate
+SELECT location, SUM(CAST(total_deaths AS int)) AS total_deaths, 
+CONCAT(MAX(ROUND((CAST(total_deaths AS bigint)/CAST(total_cases AS bigint))*100, 2)), '%') AS highest_death_rate
 FROM covid_death
 WHERE continent IS NOT NULL
 GROUP BY location
@@ -50,30 +53,41 @@ order by total_cases, total_deaths
 SELECT YEAR(date) AS year, MONTH(date) AS month, 
 SUM(CAST(total_cases AS bigint))AS total_cases,
 SUM(CAST(total_deaths AS bigint)) AS total_deaths, 
-CONCAT((ROUND(((SUM(CAST(total_deaths AS bigint))/SUM(CAST(total_cases AS bigint)))*100),10)),'%') AS death_rates
+CONCAT((CONVERT(float, ((SUM(CAST(total_deaths AS bigint))/SUM(CAST(total_cases AS bigint)))*100))),'%') AS death_rates
 FROM covid_death
 WHERE continent IS NOT NULL
 GROUP BY YEAR(date), MONTH(date)
 ORDER BY YEAR(date), MONTH(date) asc
 
--- Rolling vaccinated percentage (with cte)
+-- Rolling vaccinated percentage (with new table because the main dataset exceeds the bytes allowance)
 
-WITH rolling_vacc AS 
-(SELECT d.continent, d.location, d.date, d.population,
-v.new_vaccinations, 
-SUM(CONVERT(int, v.new_vaccinations)) OVER(PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_people_vaccinated
+DROP TABLE IF EXISTS rolling_vacc
+CREATE TABLE rolling_vacc
+(continent varchar(100),
+location varchar(100),
+date datetime,
+population numeric, 
+new_vaccinations numeric)
+
+INSERT INTO rolling_vacc
+SELECT d.continent, d.location, d.date, d.population,
+v.new_vaccinations
 FROM covid_death d
 INNER JOIN covid_vaccine v
 ON d.location = v.location
 AND d.date = v.date
-WHERE d.continent IS NOT NULL)
+WHERE d.continent IS NOT NULL
 
-SELECT continent, location, date, population,
-CONCAT(FORMAT((((rolling_people_vaccinated/population)*100)), '0.000'), '%') AS percentage_of_vaccinated
-FROM rolling_vacc
-ORDER BY 1, 2, 3
+WITH rolling_vacc_1 AS 
+(SELECT continent, location, date, population,
+new_vaccinations,
+SUM((CONVERT(bigint, new_vaccinations))) OVER(PARTITION BY location ORDER BY location, date)
+AS rolling_people_vaccinated
+FROM rolling_vacc)
 
--- Create table
+SELECT * FROM rolling_vacc_1
+
+-- Create a new table
 DROP TABLE IF EXISTS percent_of_vaccinated
 CREATE TABLE percent_of_vaccinated
 (continent varchar(100),
@@ -108,9 +122,12 @@ ORDER BY fully_vaccinated_percentage desc
 
 -- Relationship between vaccinated and gdp
 SELECT location, gdp_per_capita, 
-MAX(ROUND(CAST(((people_vaccinated/population)*100) AS bigint), 2)) AS vaccinated_percentage
+ROUND((CAST((((MAX(people_vaccinated))/(MAX(population)))*100) AS bigint)), 4) AS vaccinated_percentage
 FROM percent_of_vaccinated
 WHERE continent IS NOT NULL
 AND gdp_per_capita IS NOT NULL
 GROUP BY location, gdp_per_capita
 ORDER BY gdp_per_capita desc, vaccinated_percentage desc
+
+
+ 
